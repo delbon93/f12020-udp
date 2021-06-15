1279d3f1a54f6d674c0d3fb36728ad473569ecb8
David Holzapfel <delbon93@gmail.com>
2021-06-15 18:25:37 +0200

    Can now be run with different app targets

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-MAIN=./src/main.py
+MAIN=main
 PYTHON=./venv/bin/python
 
 add_to_path() {
@@ -16,5 +16,29 @@ add_to_path() {
 add_to_path "./src"
 export PYTHONPATH
 
+if [[ $# -gt 1 ]] ; then
+    echo "Usage: $0 [app]"
+    exit 1
+fi
+
+APP=$MAIN
+
+if [[ $# == 1 ]] ; then
+    APP=$1
+fi
+
 source ./venv/bin/activate
-$PYTHON $MAIN
+
+for APPPATH in `echo $PYTHONPATH | sed -e "s/:/ /g"` 
+do
+    TARGET=$APPPATH/$APP.py
+    ls -s $TARGET >> /dev/null 2>&1
+    if [[ $? == 0 ]] ; then
+        echo "%> Running target '$TARGET'..."
+        $PYTHON $TARGET
+        exit
+    fi
+done
+
+echo "%> Fatal: target '$APP' not found"
+exit 2
